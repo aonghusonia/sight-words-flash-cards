c1f3d116d9ded66531291a6899eca794963337ab
Aonghus O Nia <aonghusonia@eircom.net>
2019-03-23 12:14:51 -0400

    change random word picker to pick latter words more often

--- sight_words.js
+++ sight_words.js
@@ -196,7 +196,11 @@ const words = [
 
 //const getRandomWord = () => words[Math.floor(Math.random()*words.length)];
 
-const getRandomWord = () => words[Math.floor((1-Math.abs(Math.random()-Math.random()))*words.length)]
+const getRandomWord = () => {
+  var result = Math.random();
+  return words[Math.floor((result > Math.random() ? result : Math.random()) * words.length)];
+};
+
 
 
 const getNewWord = word => {
